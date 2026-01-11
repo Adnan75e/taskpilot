eb17ba6073721ed7f4827f1226deaeca19644d60
Sheril Ansari <sherilansari01@gmail.com>
2026-01-11 05:20:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9963589179-1b170",
+  "appId": "1:1081576818536:web:6a858c8963e3eacf6678dd",
+  "apiKey": "AIzaSyAx_zNtA6iYVDaUsqRmQYP5FWNyoQwcn0o",
+  "authDomain": "studio-9963589179-1b170.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1081576818536"
+};
